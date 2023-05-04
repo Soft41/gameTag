2c38561a3190ee12b4371d1d29f062587218c043
Yaroslav Mykhailychenko <arostar197541@gmail.com>
2023-05-04 14:23:12 +0300

    Add board construct/display method

--- src/Board/Board.cpp
+++ src/Board/Board.cpp
@@ -1,5 +1,41 @@
-//
-// Created by Ярослав on 04.05.2023.
-//
-
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 #include "Board.h"
+
+using namespace std;
+
+Board::Board(int x, int y) {
+    width = x;
+    height = y;
+    emptyX = 0;
+    emptyY = 0;
+    //Seed for srand
+    srand(time(NULL));
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            int value = i * width * j + 1;
+            if (value == width * height) {
+                value = 0;
+                emptyY = i;
+                emptyY = j;
+            }
+            cells[i][j] = Cell(i,j,value);
+        }
+    }
+}
+
+void Board::display() {
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            if (cells[i][j].getValue == 0) {
+                 cout << " ";
+            } else {
+                cout << cells[i][j].getValue << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
--- src/Board/Board.h
+++ src/Board/Board.h
@@ -4,10 +4,21 @@
 
 #ifndef GAMETAG_BOARD_H
 #define GAMETAG_BOARD_H
+#include "./../Cell/Cell.h"
 
+const int MAX_HEIGHT = 4;
+const int MAX_WIDTH = 4;
 
 class Board {
-
+private:
+    short height;
+    short width;
+    short emptyX;
+    short emptyY;
+    Cell cells[MAX_HEIGHT][MAX_WIDTH];
+public:
+    Board(int x, int y);
+    void display();
 };
 
 
